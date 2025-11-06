13f4074561cb738cd3bdfa0bbc91772619cdfb50
lengyue1084 <95695864@qq.com>
2025-11-06 19:13:26 +0800

    fix(deploy): 更新部署脚本以拉取最新镜像
    - 将 docker push 命令更改为 docker pull 命令- 在 docker-compose 命令之间添加空行以改善可读性

--- restart.sh
+++ restart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker push crpi-puijznmazn81i1ou.cn-hangzhou.personal.cr.aliyuncs.com/test_huizhi/flow-bridge-mcp:latest
+docker pull crpi-puijznmazn81i1ou.cn-hangzhou.personal.cr.aliyuncs.com/test_huizhi/flow-bridge-mcp:latest
 
 docker-compose down
+
 docker-compose up -d
